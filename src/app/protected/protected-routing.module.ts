600825e7b82a9dd5cc04b29ae3354220cfa3c94b
Santiago <santtiago49@gmail.com>
2022-03-07 12:01:17 -0300

    Rutas y LazyLoad

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: '**', redirectTo: '' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
