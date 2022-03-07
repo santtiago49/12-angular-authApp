f49dd59aa8826f4146ec615a6ff3301bc2010ce1
Santiago <santtiago49@gmail.com>
2022-03-07 13:43:18 -0300

    Diseño de la pantalla Login y Registro

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    email   : ['', [Validators.required, Validators.email] ],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
   }
 
 }
